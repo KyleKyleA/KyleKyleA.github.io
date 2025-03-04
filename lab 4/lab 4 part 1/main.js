65754ac43cfc01a70cb335113b4301555863e11e
KyleAngeles <kyle.angeles@dcmail.ca>
2025-03-04 17:02:16 -0500

    added the variables to the javascript file

--- lab 4/lab 4 part 1/main.js
+++ lab 4/lab 4 part 1/main.js
@@ -9,15 +9,15 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-2. RAW TEXT STRINGS
 
-It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
 
-Willy the Goblin
-Big Daddy
-Father Christmas
+const storytext = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
-the soup kitchen
+const insertx = ['Willy the Goblin']
+const insertx = ['Big Daddy'];
+const insertx = ['father Christmas'];
+
+const = the soup kitchen
 Disneyland
 the White House
 
@@ -25,7 +25,7 @@ spontaneously combusted
 melted into a puddle on the sidewalk
 turned into a slug and crawled away
 
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+
 
 randomize.addEventListener('click', result);
 
