29d631967766599356847128d58fe9c915878fa5
KyleAngeles <kyle.angeles@dcmail.ca>
2025-03-04 20:47:16 -0500

    added const and fixed the strings onto the function

--- lab 4/lab 4 part 1/main.js
+++ lab 4/lab 4 part 1/main.js
@@ -13,23 +13,22 @@ function randomValueFromArray(array){
 
 const storytext = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
-const insertx = ['Willy the Goblin']
-const insertx = ['Big Daddy'];
-const insertx = ['father Christmas'];
+const insertx = ['Willy the Goblin', 'Big daddy', 'father christmas' ];
 
-const = the soup kitchen
-Disneyland
-the White House
+const inserty = ['the soup kitchen', 'Disneyland', 'the White House'];
 
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
+const insertz = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 
 
 randomize.addEventListener('click', result);
 
 function result() {
+    let newstory = storytext;
+
+    const xItem = randomValueFromArray(insertx);
+    const yItem = randomValueFromArray(inserty);
+    const zItem = randomValueFromArray(insertz);
 
   if(customName.value !== '') {
     const name = customName.value;
