7f68cdd9ff2c5f131cddf6beccf0138dd77dbe20
KyleAngeles <kyle.angeles@dcmail.ca>
2025-03-04 22:57:53 -0500

    added the uk button, and the main jaavscript to make the program functional

--- lab 4/lab 4 part 1/main.js
+++ lab 4/lab 4 part 1/main.js
@@ -12,11 +12,8 @@ function randomValueFromArray(array){
 
 
 const storytext = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-
 const insertx = ['Willy the Goblin', 'Big daddy', 'father christmas' ];
-
 const inserty = ['the soup kitchen', 'Disneyland', 'the White House'];
-
 const insertz = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 
@@ -40,12 +37,12 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
     newstory = newstory.replaceAll('94 fahrenheit',temperature )
     newstory = newstory.replaceAll('300 pounds',weight )
   }
 
-  story.textContent = ;
+  story.textContent = newstory;
   story.style.visibility = 'visible';
 }
