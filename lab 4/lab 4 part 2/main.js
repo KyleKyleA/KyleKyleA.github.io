aac023680bb88d11839a7d859df4e1bd275977e2
KyleAngeles <kyle.angeles@dcmail.ca>
2025-03-10 12:47:44 -0400

    added the array for the program now doing delcare object for alt texts

--- lab 4/lab 4 part 2/main.js
+++ lab 4/lab 4 part 2/main.js
@@ -8,50 +8,24 @@
 /**
  * Declare an array of image filenames
  */
-
+const ImageFileNames = ['pic1.jpg' , 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg' ];
 /**
- * 
+ * Declare an object for alt texts
+ */
+const altTexts = {
+    
+}
+/**
+ * Using a loop through images and add them to the thumb-bar
  */
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * Add click event listener to each image
+ */
 
 
 
