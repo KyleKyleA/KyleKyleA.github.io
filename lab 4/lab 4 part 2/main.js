e80dddb1dfa340806b7bf18bf0a353bea95b2a40
KyleAngeles <kyle.angeles@dcmail.ca>
2025-03-10 12:42:00 -0400

    listing comments for each javascript function

---
+++ lab 4/lab 4 part 2/main.js
@@ -0,0 +1,65 @@
+/**
+ * Get references to elements
+ */
+
+
+
+
+/**
+ * Declare an array of image filenames
+ */
+
+/**
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Writing a handler that runs the darken and lightn button
+ */
+btn.setAttribute("class", xxx);
+btn.textContent = xxx;
+overlay.style.backgroundColor = xxx;
