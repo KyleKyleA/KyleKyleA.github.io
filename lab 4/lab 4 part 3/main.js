189d59bd725b7a753f9a65bf17bc2334a85ff1cc
KyleAngeles <kyle.angeles@dcmail.ca>
2025-03-14 14:23:32 -0400

    create loop function and the for loop

--- lab 4/lab 4 part 3/main.js
+++ lab 4/lab 4 part 3/main.js
@@ -83,6 +83,18 @@ update() {
         randomRGB(),
         size,
     );
-    
+
+    balls.push(ball);
+  }
+
+  function loop() {
+    ctx.fillStyle = "rgb(0 0 0/ 25%)";
+    ctx.fillRect(0, 0, width, height);
+
+    for (const ball of balls) {
+        ball.draw();
+        ball.update();
+    }
+    requestAnimationFrame(loop);
   }
    
