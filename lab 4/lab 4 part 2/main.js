3a3d2a872d7fbcb56ffdad9f9d51c01d0f38ff54
KyleAngeles <kyle.angeles@dcmail.ca>
2025-03-14 19:57:07 -0400

    trying to fix the errors on why the images aren;t clickable

--- lab 4/lab 4 part 2/main.js
+++ lab 4/lab 4 part 2/main.js
@@ -1,15 +1,18 @@
 /**
  * Get references to elements
  */
-const DisplayedImage = document.querySelector('.DisplayedImage ');
-const thumbBar = document.querySelector('.thumb-Bar');
+const DisplayedImage = document.querySelector('.displayed-image ');
+const thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('.button');
+const btn = document.querySelector('.dark');
 const overlay = document.querySelector('.overlay');
 /**
  * Declare an array of image filenames
  */
-const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg' ];
+const images = [
+  'pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg',
+  'pic6.jpg', 'pic7.jpg', 'pic8.jpg'  // <-- Add more images here
+];
 /**
  * Declare an object for alt texts
  */
@@ -18,20 +21,24 @@ const alts = {
         'pic2.jpg' : 'Rock that looks like a wave',
         'pic3.jpg' : 'Purple and white pansies',
         'pic4.jpg' : 'Section of wall from a pharoahs\'s tomb',
-        'pic5.jpg' : 'Large moth on a leaf'
+        'pic5.jpg' : 'Large moth on a leaf',
+        'pic6.jpg' : 'Sunset over mountains',
+        'pic7.jpg' : 'City skyline at night',
+        'pic8.jpg' : 'Beautiful beach with palms'
 };
 /**
  * Using a loop through images and add them to the thumb-bar
  */
 for (const image of images) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', `images/${images}`);
-    newImage.setAttribute('alt', alts[image]);
-    thumbBar.appendChild(newImage);
-    newImage.addEventListener('click', e => {
-        DisplayedImage.src = e.target.src;
-        DisplayedImage.alt = e.target.alt;
-    })
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+
+  newImage.addEventListener('click', (e) => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+  });
 }
 /**
  * Writing a handler that runs the darken and lightn button
